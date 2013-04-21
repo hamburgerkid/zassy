d574c0da5f26c881dbc19a4a4cdc7c17b427306f
hamburgerkid <hamburgerkid@gmail.com>
2013-04-22 03:54:40 +0900

    Add about page as modal window and so on.

--- js/zassy.js
+++ js/zassy.js
@@ -2,8 +2,9 @@ var column = 6
 var count = 0
 var page = 1
 var firstLimit = 3
+var hit = 20
 var above = 100
-var pageLimit = 10
+var pageLimit = 100
 var duration = 800
 
 var checkGenre = function(booksGenreId) {
@@ -14,7 +15,7 @@ var checkGenre = function(booksGenreId) {
 };
 
 var loadItem = function() {
-  $.getJSON('https://app.rakuten.co.jp/services/api/BooksMagazine/Search/20121128?applicationId=bd7dad817cb2f7fab2890e0b513db00d&affiliateId=11159626.d2e91abc.11159627.079be713&booksGenreId=007&hits=20&page=' + page + '&chirayomiFlag=0&sort=-releaseDate&elements=booksGenreId,affiliateUrl,largeImageUrl', function(data) {
+  $.getJSON('https://app.rakuten.co.jp/services/api/BooksMagazine/Search/20121128?applicationId=bd7dad817cb2f7fab2890e0b513db00d&affiliateId=11159626.d2e91abc.11159627.079be713&booksGenreId=007&hits=' + hit + '&page=' + page + '&chirayomiFlag=0&sort=-releaseDate&elements=booksGenreId,affiliateUrl,largeImageUrl', function(data) {
     $.each(data.Items, function(i, items) {
       $.each(items, function(j, item) {
         if (count % column === 0) {
@@ -42,6 +43,7 @@ $(document).ready(function() {
     loadItem()
     ++page
   }
+  return false
 });
 
 var moreItem = function() {
@@ -51,6 +53,7 @@ var moreItem = function() {
   } else if (page === pageLimit) {
     var divMsg = '<div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>No More Zassy. Thanks.</strong></div>'
     $(divMsg).appendTo('#message');
+    $('#more-item').hide();
     ++page
   }
   return false
